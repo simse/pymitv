b97fa1ba374114d8942ae900abaf025a43ae5fbb
Simon Sørensen <teamangryghast@gmail.com>
2018-02-21 12:43:33 +0100

    Updated Github URL.

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(
     # For a discussion on single-sourcing the version across setup.py and the
     # project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.2.0',  # Required
+    version='1.2.1',  # Required
 
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
@@ -61,7 +61,7 @@ setup(
     #
     # This field corresponds to the "Home-Page" metadata field:
     # https://packaging.python.org/specifications/core-metadata/#home-page-optional
-    url='https://github.com/fattdev/pymitv',  # Optional
+    url='https://github.com/simse/pymitv',  # Optional
 
     # This should be your name or the name of the organization which owns the
     # project.
